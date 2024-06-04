4a7c75721bf3502759794c752df188bbbae54293
Gabriel_Asevedo <gabrielamorim002@gmail.com>
2024-06-04 10:12:09 -0300

    class update

--- js/calcDebt.js
+++ js/calcDebt.js
@@ -1,11 +1,27 @@
-function calcDebt(){
-    const button = document.getElementById('calcDebt')
+import { debts } from './createForm.js';
+
+function calcDebt() {
+    const button = document.getElementById('calcDebt');
 
     button.addEventListener('click', () => {
-        console.log("Clicado")
-    })
+        if (debts.length === 0) {
+            alert("Nenhuma dívida foi adicionada.");
+            return;
+        }
 
+        let maxDebt = debts[0];
+
+        for (let i = 1; i < debts.length; i++) {
+            if (debts[i].value > maxDebt.value) {
+                maxDebt = debts[i];
+            }
+        }
+
+        alert(`A maior dívida é ${maxDebt.name} com valor de R$${maxDebt.value.toFixed(2)}`);
+    });
 }
+
+calcDebt();
 
 // Esta função servira para calcular os valores de todas as contas que forem geradas, este botão já está em conexão com o botão do front, 
 // preciso descobrircomo ele vai pegar os valores de cada campo e junta-los dentro de um Array, é bom ter uma regra para tirar qualquer 
@@ -13,6 +29,3 @@ function calcDebt(){
 // simplesmente fazer como eu mostrei ontem "array.sort().reverse()", trazendo para nós o maior valor do array, mas precisamos lembrar que ele
 // tem que estar vinculado com um id por exemplo ou o nome do item que o cara colocou, para retornarmos o nome da conta mais alta dele. 
 // Isso é só um exemplo, acho que tem mais coisas que eu posso fazer, mas se quiser ir adiantando algo, só mexer.
-
-
-calcDebt()
